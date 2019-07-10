8c48c2a7cf2c3a7c3fff65ce8d5463e13df7a2c0
Thousand <tmddjs9565@gmail.com>
2019-07-10 15:10:25 +0900

    restart commit
    D'oh!!

---
+++ DonateKnowHow/src/main/java/com/doh/domain/CBoardDTO.java
@@ -0,0 +1,16 @@
+package com.doh.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+
+@Data
+public class CBoardDTO {
+	private int c_no;
+	private int m_no;
+	private String c_title;
+	private String c_content;
+	private Date c_rdate;
+
+}
---
+++ DonateKnowHow/src/main/java/com/doh/mapper/CBoardMapper.java
@@ -0,0 +1,14 @@
+package com.doh.mapper;
+import java.util.List;
+import com.doh.domain.CBoardDTO;
+
+public interface CBoardMapper {
+	public List<CBoardDTO> getList();
+	public void insert(CBoardDTO cbdto);
+	public CBoardDTO read(int c_no);
+	public int delete(int c_no);
+	public int update(CBoardDTO cbdto);
+	
+	
+
+}
