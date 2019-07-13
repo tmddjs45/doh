4bdf20a53510331daee3b6f0ec4fc4ce9ce52afd
kyu9999 <51218026+kyu9999@users.noreply.github.com>
2019-07-14 05:29:02 +0900

    ;;루트컨텍스트뺌
    ;;;;

--- DonateKnowHow/src/main/java/com/doh/controller/CBoardController.java
+++ DonateKnowHow/src/main/java/com/doh/controller/CBoardController.java
@@ -1,6 +1,8 @@
 
 package com.doh.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.doh.domain.CBoardDTO;
 import com.doh.service.CBoardService;
@@ -21,6 +24,7 @@ import lombok.extern.java.Log;
 @Log
 @RequestMapping("/cboard/*")
 @AllArgsConstructor
+
 public class CBoardController {
 	@Setter(onMethod_=@Autowired)
 	private CBoardService service;
@@ -57,6 +61,7 @@ public class CBoardController {
 	@PostMapping("/update")
 	public String update(@RequestParam("c_no") int c_no,CBoardDTO cbdto, Model model) {
 		log.info("##update----");
+		String c_title="ddd";
 		
 		service.update(cbdto);
 		
@@ -81,6 +86,42 @@ public class CBoardController {
 		model.addAttribute("list", service.getList());
 		return "/Cboard/list";
 	}
+	@RequestMapping("/stest")
+	public String stest() {
+		System.out.println("드러옴이잉이이");
+		return "/Cboard/stest";
+	}
+	
+	@RequestMapping("/sidtest")
+	public String sidtest(@RequestParam("s_id") String s_id, Model model, HttpSession session) {
+		System.out.println("pre sidtest"+s_id);
+		session.setAttribute("s_id", s_id);
+		String sss=(String)session.getAttribute("s_id");
+		System.out.println(" sidtest : "+sss);
+		model.addAttribute("list", service.getList());
+		return "/sibal";
+	}
+	
+	@RequestMapping("/sidcheck")
+	public String sidcheck(HttpSession session) {
+		String sid= (String)session.getAttribute("s_id");
+		System.out.println("sidcheck : "+sid);
+		
+		return null;
+	}
+	
+	@RequestMapping("/sibal")
+	public String si(HttpSession session) {
+		String si = (String)session.getAttribute("s_id");
+		System.out.println("이건안되면 집감"+ si);
+		return null;
+	}
+	@RequestMapping("/sremove")
+	public String sremove(HttpSession session) {
+		session.invalidate();
+		return "";
+	}
+	
 	
 	
 	
---
+++ DonateKnowHow/src/main/webapp/WEB-INF/views/Cboard/stest.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>testtest</title>
+</head>
+<body>
+
+
+
+<form action ="sidtest">
+<input type='text' name='s_id' size='30' placeholder='아이디입력'>
+<input type='text' size='30' placeholder='패스워드입력'>
+<button type="submit()" >전송 </button>
+</form> 
+</body>
+</html>
